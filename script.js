1af110111ba9358be410309c8c49a9044a517084
Heitor <heitorao32@gmail.com>
2023-11-04 15:03:48 -0300

    sidebar slide

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const button = document.querySelector('.menu')
 button.addEventListener('click', switchbar)
 
 function switchbar(){
-     const header = document.querySelector('open')
-
+     let header = document.querySelector('.open');
      header.classList.toggle('closed')
-     header.removeChild('.open ul')
+
+    
+     
 
 }
+
